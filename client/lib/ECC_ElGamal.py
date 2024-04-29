9501f28b93d28cf466a9f17f4cab9565b42678be
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-04-29 21:18:51 +0700

    feat: add function signature for ECC_Elgamal

---
+++ client/lib/ECC_ElGamal.py
@@ -0,0 +1,20 @@
+def generate_keypair():
+  pass
+
+def generate_publickey():
+  pass
+
+def generate_privatekey():
+  pass
+
+def encode_message():
+  pass
+
+def decode_message():
+  pass
+
+def encrypt():
+  pass
+
+def decrypt():
+  pass
