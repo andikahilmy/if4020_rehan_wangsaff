3052b6a9faa5989a1178ec9331c3562bc4d9b526
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-05-08 08:25:12 +0700

    refactor: update ecc elgamal algorithm

--- client/lib/ECC_ElGamal.py
+++ client/lib/ECC_ElGamal.py
@@ -1,21 +1,18 @@
 import secrets
+from lib.ECC import  SECP256R1,Point
 def generate_keypair():
   private_key = generate_privatekey()
   public_key = generate_publickey(private_key)
   return (public_key,private_key)
 
 def generate_publickey(private_key:int)->Point:
-  # curve = registry.get_curve('brainpoolP256r1')
-  # return private_key * curve.g
-  pass
+  return private_key * SECP256R1.get_basepoint()
 
 def generate_privatekey()->int:
-  # curve = registry.get_curve('brainpoolP256r1')
-  # private_key =  secrets.randbelow(curve.field.n)
-  # while private_key == 0:
-  #   private_key =  secrets.randbelow(curve.field.n)
-  # return private_key
-  pass
+  private_key = secrets.randbelow(SECP256R1.n)
+  while private_key == 0:
+    private_key =  secrets.randbelow(SECP256R1.n)
+  return private_key
 
 def encode_point():
   pass
@@ -24,18 +21,14 @@ def decode_point():
   pass
 
 def encrypt(plaintext:bytes,public_key:Point):
-  # P_M = encode_point(plaintext)
-  # curve = registry.get_curve('brainpoolP256r1')
-  # k = secrets.randbelow(curve.field.n)
-  # while k == 0:
-  #   k =  secrets.randbelow(curve.field.n)
-  # c1 = k * curve.g
-  # c2 = P_M + k * public_key
-  # return (c1,c2)
-  pass
+  P_M = encode_point(plaintext)
+  k = secrets.randbelow(SECP256R1.n)
+  while k == 0:
+    k =  secrets.randbelow(SECP256R1.n)
+  c1 = k * SECP256R1.get_basepoint()
+  c2 = P_M + k * public_key
+  return (c1,c2)
 
 def decrypt(c1:Point,c2:Point,private_key:int):
-  # curve = registry.get_curve('brainpoolP256r1')
-  # P_M =  c2 - (curve.field.n -  private_key) * c1
-  # return decode_point(P_M)
-  pass
+  P_M =  c2 - (SECP256R1.n - private_key) * c1
+  return decode_point(P_M)
