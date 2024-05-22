1b8903c7b5bcc40926d99428432984804064b06f
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-05-22 22:07:12 +0700

    chore: remove unused comment

--- server/main.py
+++ server/main.py
@@ -1,27 +1,3 @@
-# from fastapi import FastAPI
-
-# app = FastAPI(port=args.port)
-
-# @app.get('/')
-# async def root():
-#   return {"message":"Hello World!"}
-
-
-# @app.post('/connect')
-# # buat handshake
-# async def connect():
-#   pass
-
-# @app.post('/transfer')
-# # buat nerima pesan dari klien A dan ngirim ke klien B
-# async def transfer():
-#   pass
-
-# @app.get("/digital-signature-param")
-# # buat dapatin global variabel digital signature
-# async def digital_signature_param():
-#   pass
-
 import asyncio
 from websockets.server import serve
 import argparse
