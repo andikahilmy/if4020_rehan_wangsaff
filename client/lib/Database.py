76ecb6db479ad8c2c55af6941a38f0f6082f4747
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-05-22 14:11:17 +0700

    refactor: update db format

--- client/lib/Database.py
+++ client/lib/Database.py
@@ -4,6 +4,7 @@ import uuid
 import hashlib
 from .ECC_ElGamal import generate_keypair
 
+
 class Database:
     DATABASE_PATH = "client/db/client.db"
 
@@ -13,54 +14,61 @@ class Database:
 
     @staticmethod
     def init_table():
-        if(not os.path.exists(Database.DATABASE_PATH)):
+        if not os.path.exists(Database.DATABASE_PATH):
             db = sqlite3.connect(Database.DATABASE_PATH)
             db.execute(
-            """create table user(
+                """
+            create table user(
 id varchar(36) primary key not null,
 name text,
 password text,
 pubkey text,
 privkey text);"""
-        )
+            )
             db.execute(
-            """CREATE TABLE chat(
+                """
+            CREATE TABLE chat(
   chat_id varchar(36) not null,
-  user_id varchar(36) not null,
+  src_port integer not null,
   content text,
   date timestamp default CURRENT_TIMESTAMP,
-  PRIMARY KEY (chat_id,user_id),
-  FOREIGN KEY(user_id) REFERENCES user(id)                   
+  PRIMARY KEY (chat_id)               
     );
 """
-        )
+            )
             db.close()
 
     @staticmethod
-    def get_message(user_id:str):
+    def get_message(src_port: int):
         db = sqlite3.connect(Database.DATABASE_PATH)
-        db.cursor().execute("SELECT * FROM chat WHERE user_id=?",(user_id))
+        db.cursor().execute("SELECT * FROM chat WHERE src_port=?", (src_port))
         output = db.cursor().fetchall()
         db.commit()
         db.close()
         return output
 
     @staticmethod
-    def add_message(user_id:str,content:str):
+    def add_message(port: int, content: str):
         db = sqlite3.connect(Database.DATABASE_PATH)
         chat_id = uuid.uuid4()
-        db.cursor().execute("INSERT INTO chat VALUES(?,?,?)",(str(chat_id),user_id,content))
+        db.cursor().execute(
+            "INSERT INTO chat VALUES(?,?,?)", (str(chat_id), port, content)
+        )
         db.commit()
         db.close()
-    
+
     @staticmethod
-    def add_user(name:str,password:str):
-        public_key,private_key = generate_keypair()
+    def add_user(name: str, password: str):
+        public_key, private_key = generate_keypair()
         db = sqlite3.connect(Database.DATABASE_PATH)
         user_id = uuid.uuid4()
-        db.cursor().execute("INSERT INTO user VALUES(?,?,?,?,?)",(str(user_id),name,password,str(public_key),str(private_key)))
+        db.cursor().execute(
+            "INSERT INTO user VALUES(?,?,?,?,?)",
+            (str(user_id), name, password, str(public_key), str(private_key)),
+        )
         db.commit()
         db.close()
+
     # @staticmethod
     # def add_public_key(user_id:str,pubkey:str):
     #     db = sqlite3.connect(Database.DATABASE_PATH)
@@ -69,13 +77,18 @@ privkey text);"""
     #     db.close()
 
     @staticmethod
-    def search_user_by_credential(name:str,password:str):
+    def search_user_by_credential(name: str, password: str):
         db = sqlite3.connect(Database.DATABASE_PATH)
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM user WHERE name=? AND password=?",(name,hashlib.sha256(password.encode()).hexdigest()))
+        cursor.execute(
+            "SELECT * FROM user WHERE name=? AND password=?",
+            (name, hashlib.sha256(password.encode()).hexdigest()),
+        )
         output = cursor.fetchone()
         db.commit()
         db.close()
         return output
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     d = Database()
