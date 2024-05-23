9c52e5a121427a43f19f2c4eba5034f2b41980da
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-05-23 22:10:53 +0700

    feat: websocket able to communicate between client server

--- server/lib/ClientManager.py
+++ server/lib/ClientManager.py
@@ -1,13 +1,18 @@
 from fastapi import WebSocket
+import logging
 class ClientManager():
   def __init__(self) -> None:
     self.CONNECTIONS:dict[int,tuple] = {} #Key:PORT, value:(WebSocket, shared key)
+    self.logger = logging.getLogger('uvicorn')
 
-  async def connect(self,port:int,websocket:WebSocket,shared_key):
-    self.CONNECTIONS[port] = (websocket,shared_key)
+  def connect(self,websocket:WebSocket,shared_key):
+    self.CONNECTIONS[websocket.client.port] = (websocket,shared_key)
 
-  async def disconnect(self,port:int):
+  def disconnect(self,port:int):
     del self.CONNECTIONS[port]
 
   async def send_message(self,dst_port,message:str):
-    self.CONNECTIONS[dst_port][0].send_text(message)
+    try:
+      self.CONNECTIONS[dst_port][0].send_text(message)
+    except KeyError:
+      self.logger.error("Client not found")
--- server/main.py
+++ server/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI,WebSocket,WebSocketDisconnect
 import json
+from .lib.ClientManager import ClientManager
+import logging
+
+
+# Setting Logging
+logger = logging.getLogger('uvicorn')
+logger.setLevel(logging.DEBUG)
 
 CONNECTIONS = {}
-# print(args.port)
+manager = ClientManager()
 app = FastAPI()
 
 @app.get('/')
@@ -12,9 +19,19 @@ async def root():
 
 @app.websocket('/connect')
 # buat handshake
-async def connect():
-  #TODO handshake
-  pass
+async def connect(ws:WebSocket):
+  await ws.accept()
+  logger.info(f"Received connection from {ws.client.host}:{ws.client.port}")
+  try:
+    while True:
+      data = await ws.receive_text()
+      #TODO Handshake
+      shared_key = ""
+      manager.connect(ws,shared_key)
+      # TODO handshake: Konfirmasi
+      await ws.send_text("Connection Established")
+  except WebSocketDisconnect:
+    logger.info(f"Client {ws} disconnected")
 
 @app.websocket('/messaging')
 # buat nerima pesan dari klien A dan ngirim ke klien B
@@ -26,11 +43,11 @@ async def messaging(ws:WebSocket):
       #TODO decrypt data
       # Parse ke json
       message = json.loads(data)
-      print("Received message:",message,"from",ws)
+      logger.info(f"Received message: {message} from {ws}")
       #TODO kirim ke B
       await ws.send_json(message)
   except WebSocketDisconnect:
-    print(f"Client {ws} disconnected")
+    logger.info(f"Client {ws} disconnected")
 
 @app.get("/digital-signature-param")
 # buat dapatin global variabel digital signature
